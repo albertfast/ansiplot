39718c2df3ce35fdbc55feb8a33fd47dd0eac886
albertfast <ahmethasimsahiner@icloud.com>
2025-04-19 06:28:27 -0700

    Added setup.py for pip install support

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ansiplot',
+    version='0.1.3-ahmet',
+    description='Terminal plotting with vertical histogram and bar chart labels by Ahmet Sahiner',
+    author='Ahmet Sahiner',
+    author_email='ahmethasimsahiner@icloud.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
+
