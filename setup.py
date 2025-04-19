cb03a26dceb9fd6b8f2cd954b575d9c5d6ac7639
albertfast <ahmethasimsahiner@icloud.com>
2025-04-19 06:31:05 -0700

    Fix version format to PEP 440 compliant

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ansiplot',
-    version='0.1.3-ahmet',
+    version='0.1.3+ahmet',
     description='Terminal plotting with vertical histogram and bar chart labels by Ahmet Sahiner',
     author='Ahmet Sahiner',
     author_email='ahmethasimsahiner@icloud.com',
